6a3a892e5630ea50bb679f38fb334db9ec8ead87
kaushik94 <kaushik.varanasi1@gmail.com>
2015-08-14 01:25:56 +0530

    endpoint accepting list

--- feed.js
+++ feed.js
@@ -1,17 +1,39 @@
 var interval,
     onChangeHandler;
 
-var stocks = [];
+var streamingData = [ {
+    "_type": "tweet",
+    "_id": "3",
+    "_source": {
+    "user": "asdf",
+    "message": "at the Zbottom"
+    }
+  },
+  {
+    "_type": "tweet",
+  "_id": "1",
+  "_source": {
+    "user": "sid",
+    "message": "at the top"
+    }
+  }
+  ];
 
 
-function getStreamingData() {
+function getStreamingData(sdata) {
 
     // Get streaming data from a url continously !
+
+  console.log(sdata);
+  for(var each in sdata) {
+    onChangeHandler(sdata[each]._type, "stock", sdata[each]);
+  }
+
 }
 
 function start(onChange) {
     onChangeHandler = onChange;
-    interval = setInterval(getStreamingData, 200);
+    interval = setInterval(function() { getStreamingData(streamingData); }, 200);
 }
 
 function stop() {
