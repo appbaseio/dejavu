6116d0e890b8780abcc8672d98577feb607d0766
kaushik94 <kaushik.varanasi1@gmail.com>
2015-09-09 01:43:42 +0530

    commenting main.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,27 @@
  * do so by flowing back the data and calling the `resetData` function
  * here. This is sort of like the Darth Vader - Dangerous and
  * Commands everything !
-
+ *
  * ref: https://facebook.github.io/react/docs/two-way-binding-helpers.html
  */
 
 var HomePage = React.createClass({
+    /*
+     * The underlying data structure that holds the documents/records
+     * is a hashmap with keys as '_id + _type'(refer to keys.js). Its
+     * because no two records can have the same _type _id pair, so its
+     * easy to check if a record already exists.
+     */
     getInitialState: function() {
         return {documents: [{}], types: []};
     },
+    /*
+     * The record might have nested json objects. The can't be shown
+     * since it looks cumbersome in the table. What we do in the case
+     * of a nested json object is, we replace it with a font-icon( in
+     * injectLink) which upon clicking shows a Modal with the json
+     * object it contains.
+     */
     flatten: function(data, callback) {
         var fields = [];
         for(var each in data){
@@ -34,6 +47,8 @@ var HomePage = React.createClass({
             delete data['_source'];
         if(data['_index'])
             delete data['_index'];
+        if(data['score'])
+            delete data['score'];
         return callback(data, fields);
     },
     showJSON: function(data, event){
@@ -56,6 +71,13 @@ var HomePage = React.createClass({
     deleteRow: function(index){
         delete sdata[index];
     },
+    /*
+     * We cannot render a hashmap of documents on the table,
+     * hence we convert that to a list of documents every time
+     * there is a delete/update/chaange. This can be more optimised
+     * later but it is not that expensive right now, read writes to 
+     * DOM are much more expensive.
+     */
     resetData: function(){
         sdata_values = [];
         for(each in sdata){
@@ -63,12 +85,27 @@ var HomePage = React.createClass({
         }
         this.setState({documents: sdata_values});
     },
+
+    /* Logic to stream continuous data
+     * We call the getData function in feed.js
+     * which returns an update which is a single
+     * json document(record).
+     */
     getStreamingData: function(typeName){
-        // Logic to stream continuous data
         feed.getData(typeName, function(update){
             update = this.flatten(update, this.injectLink);
             var key = rowKeyGen(update);
+            /* 
+             * If the record already exists in sdata, it should
+             * either be a delete request or a change to an 
+             * existing record.
+             */
             if(sdata[key]){
+                /*
+                 * If the update has a '_deleted' field, apply
+                 * a 'delete transition' and then delete
+                 * the record from sdata.
+                 */
                 if(update['_deleted']){
                     for(var each in sdata[key]){
                             var _key = keyGen(sdata[key], each);
@@ -82,6 +119,13 @@ var HomePage = React.createClass({
                         }.bind(null, this.resetData)
                     , 1100);
                 }
+                /*
+                 * If it isn't a delete, we should find a record
+                 * with the same _type and _id and apply an 'update
+                 * transition' and then update the record in sdata.
+                 * Since sdata is modeled as a hashmap, this is 
+                 * trivial.
+                 */
                 else{
                     sdata[key] = update;
                     this.resetData();
@@ -93,6 +137,10 @@ var HomePage = React.createClass({
                     updateTransition(key);
                 }
             }
+            /*
+             * If its a new record, we add it to sdata and then
+             * apply the 'new transition'.
+             */
             else{
                     sdata[key] = update;
                     this.resetData();
@@ -105,8 +153,8 @@ var HomePage = React.createClass({
             }
         }.bind(this));
     },
+    // only called on change in types.
     getStreamingTypes: function(){
-        // only called on change.
         feed.getTypes( function(update){
             this.setState({types: update});
         }.bind(this));
@@ -138,10 +186,17 @@ var HomePage = React.createClass({
         var upar = elem.scrollTop;
             scroll = elem.offsetHeight;
             niche = elem.scrollHeight;
+        // Plug in a handler which takes care of infinite scrolling
         if(upar + scroll >= niche){
             console.log("bottom");
         }
     },
+    /*
+     * The homepage is built on two children components(which may
+     * have other children components). TypeTable renders the 
+     * streaming types and DataTable renders the streaming types.
+     * main.js ties them together.
+     */
     render: function () {
         return (
             <div>
