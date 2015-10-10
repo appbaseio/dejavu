e622cf7cbb303affde77480b26e7d90be64199bd
kaushik94 <kaushik.varanasi1@gmail.com>
2015-10-10 21:45:02 +0530

    docs for dropdown.js

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,8 +1,16 @@
+/*
+ * This file contains the utility components, mainly the dropdown menu for
+ * selecting which fields to apphear in the table, also the checkboxes component
+ * for selecting and deselecting the columns(rendered inside the dropdown).
+ */
+
 var Dropdown = React.createClass({
     render: function(){
         var Dropdown = ReactBootstrap.Dropdown;
         var columns = this.props.cols;
         var MenuItem = ReactBootstrap.MenuItem;
+        // We'll get the dropdown menu items i.e. checkboxes
+        // as a list of react components i.e FieldCheckbox.
         var ColumnsCheckbox =  columns.map(function(item){
             var key = dropdownKeyGen(item);
             return <FieldCheckbox _type={item} _key={this.key}/>;
@@ -24,11 +32,18 @@ var Dropdown = React.createClass({
 }
 });
 
+// The problem with checkboxes in bootstrap and react-bootstrap while using
+// them in react is that if you keep the fields checked by default, the don't
+// uncheck like I mean forever. This is an issue in the react-bootstrap.
 var FieldCheckbox = React.createClass({
     getInitialState: function(){
         var elemID = this.props._type;
         var checked = true;
         var elem = document.getElementById(elemID);
+        // the elem here is the column header i.e the type.
+        // If its not visible, keep the checkbox by default
+        // unckecked. This is useful when you want to preserve
+        // your state even after refresh.
         if(!elem)
             return {isChecked: checked};
         if(elem.style.display === "none"){
@@ -36,12 +51,18 @@ var FieldCheckbox = React.createClass({
         }
         return {isChecked: checked};
     },
+    // This is the on-click handler for the checkboxes
+    // when we check/uncheck a checkbox, all the elements
+    // which belong to that column should be visible/invisible
+    // we do this by assigning specific keys to them. check KeyGen.
     check: function(elementId, event){
         var checked = true;
         if(document.getElementById(elementId).style.display === "none"){
             document.getElementById(elementId).style.display = "";
             checked = true;
 
+            // we iterate through all the cells in that column via
+            // their id and then change their visibility.
             for(var each in sdata){
                 var key = keyGen(sdata[each], elementId);
                 document.getElementById(key).style.display = ""
