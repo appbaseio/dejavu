46269ccff17c383967ed4426ff770378da81a541
Farhan Chauhan <farhan687@gmail.com>
2016-01-21 10:15:49 +0530

    Changes according to ElasticSearch plugin guide
    1) _site directory
    2) properties file

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,43 @@ var reactify = require('reactify');
 
 gulp.task('browserify', function() {
 	var b = browserify({
-		entries: ['src/js/app.js'],
+		entries: ['_site/src/js/app.js'],
 		debug: true
 	});
 	b.transform(reactify); // use the reactify transform
 	return b.bundle()
 		.pipe(source('main.js'))
-		.pipe(gulp.dest('./dist'));
+		.pipe(gulp.dest('./_site/dist'));
 });
 
-gulp.task('watch', function() {
-	gulp.watch('src/js/*.js', ['browserify']);
-	gulp.watch('src/js/*.jsx', ['browserify']);
+gulp.task('connect', function () {
+    var connect = require('connect');
+    var app = connect()
+        .use(require('connect-livereload')({ port: 35729 }))
+        .use(connect.static('_site'))
+        .use(connect.static('.tmp'))
+        .use(connect.directory('_site'));
+
+    require('http').createServer(app)
+        .listen(1358)
+        .on('listening', function () {
+            console.log('Started connect web server on http://127.0.0.1:1358');
+        });
+});
+
+gulp.task('watch', ['connect'], function() {
+	var live = require('gulp-livereload');
+    live.listen();
+
+    gulp.watch([
+    	'_site/src/js/*.js',
+    	'_site/src/js/*.jsx'	
+    ]).on('change', function (file) {
+        live.changed(file.path);
+    });
+
+	gulp.watch('_site/src/js/*.js', ['browserify']);
+	gulp.watch('_site/src/js/*.jsx', ['browserify']);
 });
 
 gulp.task('default', ['browserify']);
