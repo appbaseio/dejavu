a0cc8dd1594770c06d333366c77fa4f1ebb70bf2
Siddharth Kothari <sids.aquarius@gmail.com>
2016-01-29 10:17:08 +0530

    updates import into appbase modal content

--- src/js/FeatureComponent.jsx
+++ src/js/FeatureComponent.jsx
@@ -297,18 +297,25 @@ var ImportData = React.createClass({
     return (
       <div>
         <a title="Import" onClick={this.open} >
-          <img src="src/img/import.png" /> Import <span className="small-span">from JSON, MongoDB, ElasticSearch</span>
+          <img src="src/img/import.png" /> Import <span className="small-span">from JSON, MongoDB</span>
         </a>
         <Modal show={this.state.showModal} onHide={this.close}>
           <Modal.Header closeButton>
-            <Modal.Title>Import Data</Modal.Title>
+            <Modal.Title>Import Data into Appbase <span className="small-span">from JSON, MongoDB</span></Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h3>Import content</h3>
+            <p>Use appbase.io's <a href="https://github.com/appbaseio/transporter" target="_new">transporter <span className="fa fa-external-link"></span></a> fork to import data from
+            MongoDB, any JSON structure, or text file in three simple steps:
+            </p>
+            <ol>
+              <li>Get the latest release for your system from
+                <a href="https://github.com/appbaseio/transporter/releases/tag/v0.1.2-appbase" target="_new"> here <span className="fa fa-external-link"></span></a>.</li>
+              <li>Set the source and sink configurations as mentioned
+              in the file here, and save it in the same folder as config.yml.</li>
+            <li>Run the transporter using ./transporter run --config <config_file> <transform_file></li>
+            </ol>
+            <p>Or shoot us at info@appbase.io or intercom if you want us to help with importing data.</p>
           </Modal.Body>
-          <Modal.Footer>
-            <Button id="close-modal" onClick={this.close}>Close</Button>
-          </Modal.Footer>
         </Modal>
       </div>
     );
