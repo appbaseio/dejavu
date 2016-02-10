cbd9e846d8a07124629f51b797520e09f92985a8
Farhan Chauhan <farhan687@gmail.com>
2016-02-10 22:08:20 +0530

    Add footer

--- site/src/js/homePage.jsx
+++ site/src/js/homePage.jsx
@@ -691,6 +691,15 @@ var HomePage = React.createClass({
                                 pageLoading={this.state.pageLoading}
                                 reloadData={this.reloadData} />
                         </div>
+                        <footer className="text-center">
+                            <a href="http://appbaseio.github.io/dejaVu">watch video</a> 
+                            <span className="text-right pull-right powered_by">
+                                Create your ElasticSearch in cloud with&nbsp;<a href="http://appbase.io">appbase.io</a>
+                            </span>  
+                            <span className="pull-left github-star">
+                                <iframe src="https://ghbtns.com/github-btn.html?user=appbaseio&repo=dejaVu&type=star&count=true" frameborder="0" scrolling="0" width="120px" height="20px"></iframe>
+                            </span>   
+                        </footer>
                     </div>
                 </div>);
     }
