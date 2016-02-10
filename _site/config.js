8406857149d8e3d1da3d9a0d4bad3aa3fc502e3b
Farhan Chauhan <farhan687@gmail.com>
2016-02-10 18:10:39 +0530

    Add gh-pages

--- _site/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-				"hostname": "scalr.api.appbase.io",
-				"appname": "App2",
-				"username": "pW2DT42Cw",
-				"password": "a4fae72b-014d-4600-bb6e-544be036db40",
-				"email":"farhan687@gmail.com",
-				"url":"https://pW2DT42Cw:a4fae72b-014d-4600-bb6e-544be036db40@scalr.api.appbase.io"
-			}
---
+++ js/custom.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+	//Resize
+	function resize() {
+		var height = $(window).height();
+		$('header').height(height);
+	}
+	resize();
+	$(window).resize(function() {
+		resize();
+	});
+
+	//Video
+	var myVideo = document.getElementById("dejavu-video");
+	var myVideoFlag = false;
+	function playPause() {
+		if (myVideo.paused) {
+			$('.js-hero').addClass('play');
+			myVideo.play();
+			myVideoFlag = true;
+		} else {
+			$('.js-hero').removeClass('play');
+			myVideo.pause();
+			myVideoFlag = false;
+		}
+	}
+	$('.video-play').on('click', playPause);
+
+	//Onscroll
+	$(window).on('scroll', function() {
+		var top = $(window).scrollTop();
+		if (top > 120) {
+			$('.dejaVu-icon-strip').addClass('fixed');
+			$('.js-hero').removeClass('play');
+		} else {
+			$('.dejaVu-icon-strip').removeClass('fixed');
+			if(myVideoFlag)
+				$('.js-hero').addClass('play');
+		}
+	});
+});
