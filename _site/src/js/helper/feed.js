0c2bb9d3395bd02048b5dae28a82f8cf25298afe
Farhan Chauhan <farhan687@gmail.com>
2016-03-04 15:03:02 +0530

    Get data size according to height of window to enable scroll

--- _site/src/js/helper/feed.js
+++ _site/src/js/helper/feed.js
@@ -2,7 +2,8 @@
 // authentication and streaming data from your
 // endpoint.
 // **Configs:** Appname and Credentials
-const DATA_SIZE = 20;
+
+const DATA_SIZE = get_data_size();
 var APPNAME, USERNAME, PASSWORD, URL;
 var appbaseRef;
 var getMapFlag = false;
@@ -24,6 +25,16 @@ var HOST =  URL.indexOf('@') != -1 ? httpPrefix[0]+'://'+pwsplit[1] : URL;
 var OperationFlag = false;
 var APPURL = URL + '/' + APPNAME;
 init();
+
+// Get data size according to window height
+function get_data_size() {
+	var mininum_data_size = 20;
+	var winHeight = $(window).height() - 150;
+	var rowHeight = 51;
+	var min_rows = Math.ceil(winHeight/rowHeight);
+	var rows = min_rows < mininum_data_size ? mininum_data_size : min_rows;
+	return rows;
+}
 
 function init() {
 
