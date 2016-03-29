37bff59bee521f2f6a0933f24100bc5e3f163ea2
Siddharth Kothari <sids.aquarius@gmail.com>
2016-03-30 00:01:15 +0530

    bump chrome extension version

--- chrome-build.sh
+++ chrome-build.sh
@@ -1,9 +1,13 @@
 vim manifest.json
 git add manifest.json
 git commit -m "bump chrome extension version"
+git push origin chrome-extension
 bower install
 cp -r . ../dejaVucopy/
 cd ../dejaVucopy
 rm -rf .git
 rm -rf node_modules
-cd ../dejaVu
+cd ../
+zip -r dejavu-new.zip dejaVucopy
+cd dejaVu
+echo "'dejavu-new.zip' is ready to be published to the chromestore"
