dc6d019f7f1e0dbbc1cdbe961e32626b812b8308
Farhan Chauhan <farhan687@gmail.com>
2016-04-29 12:35:22 +0530

    Update with master branch

---
+++ site/app/bower_components/appbase-js/src/actions/index.js
@@ -0,0 +1,34 @@
+var helpers = require('../helpers')
+
+var indexService = function indexService(client, args) {
+	var valid = helpers.validate(args, {
+		'type': 'string',
+		'body': 'object'
+	})
+	if(valid !== true) {
+		throw valid
+		return
+	}
+	var type = args.type
+	var id = args.id
+	var body = args.body
+	delete args.type
+	delete args.id
+	delete args.body
+
+	var path
+	if(id) {
+		path = type + '/' + id
+	} else {
+		path = type
+	}
+
+	return client.performStreamingRequest({
+		method: 'POST',
+		path: path,
+		params: args,
+		body: body
+	})
+}
+
+module.exports = indexService
