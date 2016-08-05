a28b8759b24b35ec0d19a2e81698d744daeac23d
Farhan Chauhan <farhan687@gmail.com>
2016-08-05 21:07:31 +0530

    Naming convention fix

--- _site/src/js/table/Table.jsx
+++ _site/src/js/table/Table.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactBootstrap = require('react-bootstrap');
-var Dropdown = require('./DropDown.jsx');
+var Dropdown = require('./Dropdown.jsx');
 var FeatureComponent = require('../features/FeatureComponent.jsx');
 var FilterDropdown = require('./FilterDropdown.jsx');
 var PageLoading = require('./PageLoading.jsx');
