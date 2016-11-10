73b36253ee423e7ec7af681278f6f9cee60bf9aa
farhan687 <farhan687@gmail.com>
2016-11-10 10:55:24 +0530

    Remove authConfig file

--- _site/src/js/authService/authConfig.jsx
+++
@@ -1,7 +0,0 @@
-var authConfig = {
-	domain: 'appbaseio.auth0.com',
-	clientID: 'tCy6GxnrsyKec3UxXCuYLhU6XWFCMgRD',
-	callbackURL: location.href,
-	callbackOnLocationHash: true
-};
-module.exports = authConfig;
