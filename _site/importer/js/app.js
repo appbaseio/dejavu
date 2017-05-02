4a180170eae26b76de632ba6fb4fec0703b20218
farhan687 <farhan687@gmail.com>
2017-05-02 13:16:44 +0530

    Set importer route url
    Set it to index.html to fix the issue with chrome-extension

--- _site/importer/js/app.js
+++ _site/importer/js/app.js
@@ -3,7 +3,7 @@ var ReactDOM = require('react-dom');
 var Importer = require('../../src/js/features/Importer.jsx');
 
 function onClose() {
-	window.location.href = "../";
+	window.location.href = "../index.html";
 }
 
 ReactDOM.render(<Importer directImporter={true} onClose={onClose} />, document.getElementById('main'));
