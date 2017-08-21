f6c35bdc4cb5a9d4599d6afaf1f096971a11fc68
Divyanshu Maithani <div.blackcat@gmail.com>
2017-08-21 14:09:40 +0530

    Add ColumnLabel component for displaying datatypes

---
+++ live/src/js/table/ColumnLabel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ColumnLabel = ({ children }) => (
+	<span className="column-label">{children}</span>
+);
+
+ColumnLabel.propTypes = {
+	children: PropTypes.element
+};
+
+export default ColumnLabel;
