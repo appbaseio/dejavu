0a7435a912420227e7114f8bba83dbd68e056331
Divyanshu Maithani <div.blackcat@gmail.com>
2017-08-21 14:10:11 +0530

    Fix some linting errors in Column and add ColumnLabel

--- live/src/js/table/Column.js
+++ live/src/js/table/Column.js
@@ -1,8 +1,9 @@
-var React = require('react');
+import React from 'react';
 import { OverlayTrigger, Popover } from 'react-bootstrap';
-var FilterDropdown = require('./FilterDropdown.js');
+import FilterDropdown from './FilterDropdown';
+import ColumnLabel from './ColumnLabel';
 
-var cellWidth = '250px';
+const cellWidth = '250px';
 
 class Column extends React.Component {
 	state = {
@@ -19,69 +20,90 @@ class Column extends React.Component {
 		});
 	}
 
-	render() {
-		
-		var item = this.props._item;
-		var type = this.state.type == null ? this.props._type : this.state.type;
-		var sortInfo = this.props._sortInfo;
-		var filterInfo = this.props.filterInfo;
+	getDatatype(datatype) {
+		switch (datatype) {
+			case 'string':
+				return 'String';
+			case 'integer':
+				return 'Integer';
+			case 'geo_point':
+				return 'Geo Point';
+			case 'boolean':
+				return 'Boolean';
+			case 'date':
+				return 'Date';
+			default:
+				return datatype;
+		}
+	}
 
-		var filterClass = filterInfo.active && filterInfo.columnName == item ? 'filterActive' : '';
-		var extraClass = sortInfo.column == item ? 'sortActive ' + sortInfo.reverse : '';
-		var fixedHead = 'table-fixed-head column_width ' + extraClass + ' ' + filterClass;
-		var filterId = 'filter-' + item;
-		var datatype = null;
-		var analyzed = true;
+	render() {
+		const item = this.props._item;
+		const type = this.state.type == null ? this.props._type : this.state.type;
+		const sortInfo = this.props._sortInfo;
+		const filterInfo = this.props.filterInfo;
+
+		const filterClass = filterInfo.active && filterInfo.columnName == item ? 'filterActive' : '';
+		const extraClass = sortInfo.column == item ? 'sortActive ' + sortInfo.reverse : '';
+		const fixedHead = 'table-fixed-head column_width ' + extraClass + ' ' + filterClass;
+		const filterId = 'filter-' + item;
+		let datatype = null;
+		let analyzed = true;
 		const prettyData = " Clicking on {...} displays the JSON data. ";
-		var itemText = item == 'json' ? 
+		const itemText = item == 'json' ?
 				(<span>
 					<OverlayTrigger trigger="click" rootClose placement="right" overlay={<Popover id="ab1" className="nestedJson jsonTitle">{prettyData}</Popover>}>
 						<a href="javascript:void(0);" className="bracketIcon"></a>
 					</OverlayTrigger>
 					<span>&nbsp;&nbsp;type / id</span>
-				</span>) : 
+				</span>) :
 				(<span onClick={this.sortingInit}>{item}
 				</span>);
-		var thtextShow = item == 'json' ? 'leftGap thtextShow' : 'thtextShow';
-		
-		//get the datatype if field is not json & type mapping has properties field
+		const thtextShow = item == 'json' ? 'leftGap thtextShow' : 'thtextShow';
+
+		// get the datatype if field is not json & type mapping has properties field
 		try {
 			if (item != 'json' && this.props.mappingObj[type] && this.props.mappingObj[type].hasOwnProperty('properties') && typeof this.props.mappingObj[type] != 'undefined' && typeof this.props.mappingObj[type]['properties'][item] != 'undefined') {
 				datatype = this.props.mappingObj[type]['properties'][item].type;
 				analyzed = this.props.mappingObj[type]['properties'][item].index == 'not_analyzed' ? false : true;
 			}
 		}
-		catch(err) {
+		catch (err) {
 			console.log(err);
 		}
 		// console.log(type, item);
-		//Allow sorting if item is not the first column
-		//here first column is  json = type/id
-		var sortIcon = (item == 'json') ? <span></span> : <span className="sortIcon"  onClick={this.sortingInit}>
-											<i className ="fa fa-chevron-up asc-icon" />
-											<i className ="fa fa-chevron-down desc-icon" />
-										</span>;
-		var filterRow = this.props.externalQueryApplied ? '' :(
-							<span className="filterIcon">
-								<FilterDropdown columnField={item} type={type} datatype = {datatype} analyzed={analyzed} filterInfo={this.props.filterInfo} />
-							</span>
+		// Allow sorting if item is not the first column
+		// here first column is  json = type/id
+		const sortIcon = (item == 'json') ? <span></span> : <span className="sortIcon"  onClick={this.sortingInit}>
+			<i className="fa fa-chevron-up asc-icon" />
+			<i className="fa fa-chevron-down desc-icon" />
+		</span>;
+		const filterRow = this.props.externalQueryApplied ? '' : (
+			<span className="filterIcon">
+				<FilterDropdown columnField={item} type={type} datatype={datatype} analyzed={analyzed} filterInfo={this.props.filterInfo} />
+			</span>
 		);
-		//console.log(datatype, item);
-		//var handleSort = this.sortingInit;
+		// const handleSort = this.sortingInit;
 
-		return (<th id={item} width={cellWidth} className="tableHead">
-					<div className={fixedHead}>
-						<div className="headText">
-							<div className={thtextShow}>
-								{itemText}
-							</div>
-							<div className="iconList">
-								{sortIcon}
-								{filterRow}
-							</div>
+		return (
+			<th id={item} width={cellWidth} className="tableHead">
+				<div className={fixedHead}>
+					<div className="headText">
+						<div className={thtextShow}>
+							{itemText}
+							{
+								datatype &&
+								<ColumnLabel>{this.getDatatype(datatype)}</ColumnLabel>
+							}
+						</div>
+						<div className="iconList">
+							{sortIcon}
+							{filterRow}
 						</div>
 					</div>
-				</th>);
+				</div>
+			</th>
+		);
 	}
 }
 
