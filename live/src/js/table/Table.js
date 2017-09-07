293633a5dc5469194a9089fbd24b1ecfa70953bc
Divyanshu Maithani <div.blackcat@gmail.com>
2017-09-07 20:43:58 +0530

    Resolve scroll issues and add sticky table header and footer

--- live/src/js/table/Table.js
+++ live/src/js/table/Table.js
@@ -1,27 +1,13 @@
-var React = require('react');
-var FeatureComponent = require('../features/FeatureComponent.js');
-var PageLoading = require('./PageLoading.js');
-var Info = require('./Info.js');
-var Column = require('./Column.js');
-var Pretty = FeatureComponent.Pretty;
-var Cell = require('./Cell.js');
+import React from 'react';
 
-// row/column manipulation functions.
-// We decided to roll our own as existing
-// libs with React.JS were missing critical
-// features.
-var cellWidth = '250px';
-
-// **Cell** defines the properties of each cell in the
-// data table.
-
+const FeatureComponent = require('../features/FeatureComponent.js');
 
 // This is another wrapper around the data table to implement
 // pagination, throbbers, styling etc.
 class Table extends React.Component {
 	componentDidMount() {
-		var elem = document.getElementById('table-scroller');
-		// WE are listning for scroll even so we get notified
+		const elem = document.getElementById('table-scroller');
+		// We are listning for scroll even so we get notified
 		// when the scroll hits the bottom. For pagination.
 		elem.addEventListener('scroll', this.props.scrollFunction);
 	}
@@ -33,16 +19,23 @@ class Table extends React.Component {
 			// elem.style.width = this.props.visibleColumns.length * column_width + 'px';
 		}
 		return (
-			<div id='table-container' className="table-container">
+			<div id="table-container" className="table-container">
 				<div id="table-scroller" className="table-scroller">
-					<table id="data-table"
-					className="table table-fixedheader table-bordered">
-						<thead id='columns'>
+					<table
+						id="data-table"
+						className="table table-fixedheader table-bordered"
+					>
+						<thead id="columns">
 							<tr>
 								{this.props.renderColumns}
 							</tr>
 						</thead>
-						<tbody className='exp-scrollable'>
+						<tbody
+							className="exp-scrollable"
+							style={{
+								maxHeight: window.innerHeight - 320
+							}}
+						>
 							{this.props.renderRows}
 						</tbody>
 					</table>
