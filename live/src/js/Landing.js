d06719c6852256d339db58e5f6c847057e5b7117
Divyanshu Maithani <div.blackcat@gmail.com>
2018-08-13 20:49:59 +0530

    Update new landing

--- live/src/js/Landing.js
+++ live/src/js/Landing.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { GithubButton, Navbar, H1, H2, Hero, Flex, Button, Text } from '@appbaseio/designkit';
+import { GithubButton, Navbar, H1, H2, H4, Hero, ImageCard, Title, Link, Flex, Grid, Button, Text, Section } from '@appbaseio/designkit';
 import { Input, Form, Icon, Select, Tag, Button as AntButton } from 'antd';
-import { css, cx } from 'react-emotion';
+import { css, cx, injectGlobal } from 'react-emotion';
 import UrlParser from 'url-parser-lite';
 import { bool, arrayOf, object, string, func } from 'prop-types';
 
@@ -24,6 +24,13 @@ const input = css({
 	fontSize: 14,
 	marginBottom: 30
 });
+
+// eslint-disable-next-line
+injectGlobal`
+	html {
+		font-size: 16px;
+	}
+`;
 
 // a new page but connections are still legacy
 class Landing extends Component {
@@ -55,7 +62,7 @@ class Landing extends Component {
 			onAppNameChange
 		} = this.props;
 		return (
-			<section css={{ height: '100%', width: '100%', background: 'white', position: 'absolute', zIndex: 10 }}>
+			<section css={{ height: '100%', width: '100%', background: 'white', position: 'absolute', zIndex: 10, overflow: 'auto' }}>
 				<Navbar height="80px">
 					<Navbar.Logo>
 						<img src="/live/assets/img/dejavu.svg" alt="Dejavu Logo" height="45px" />
@@ -181,6 +188,104 @@ class Landing extends Component {
 						<img src="/live/assets/img/down.svg" alt="Scroll Down" />
 					</Flex>
 				</Hero>
+				<Section>
+					<div>
+						<H2 css={{ marginBottom: 15 }}>Data Importer</H2>
+						<H4>Bring data from your JSON or CSV files into Elasticsearch, set mappings with a guided process. Can import up to 100,000 records at a time.</H4>
+					</div>
+				</Section>
+				<Section background="#FBFBFB">
+					<div>
+						<H2 css={{ marginBottom: 15 }}>Data Browser</H2>
+						<H4>Browse your imported data, add new fields, edit data. Create filter views, query views and export the data back as JSON or CSV.</H4>
+					</div>
+				</Section>
+				<Section>
+					<div>
+						<H2 css={{ marginBottom: 15 }}>Search Sandbox</H2>
+						<H4>Build the search UI/Ux visually, set filters and result parameters. Export the code to get a functional React web app.</H4>
+					</div>
+				</Section>
+				<Section background="#FBFBFB" innerSectionProps={{ columns: '1fr', className: css({ flex: 1 }) }}>
+					<div>
+						<H2 center css={{ marginBottom: 15 }}>Browse Dejavu Datasets</H2>
+						<H4 center>Browse the following public Dejavu datasets, you can clone them or submit your own dataset.</H4>
+					</div>
+					<Grid
+						size={4}
+						mdSize={2}
+						smSize={1}
+						gutter="15px"
+						smGutter="0px"
+					>
+						<ImageCard big src="/live/assets/img/airbed.png">
+							<div>
+								<Title>Airbeds data set</Title>
+								<p>
+									A dataset for building an Airbnb
+									style house aggregation search.
+								</p>
+							</div>
+							<div>
+								<Link
+									primary
+									href="https://opensource.appbase.io/reactivesearch/demos/airbeds/"
+								>
+									Browse
+								</Link>
+							</div>
+						</ImageCard>
+						<ImageCard big src="/live/assets/img/technews.png">
+							<div>
+								<Title>Hacker News data set</Title>
+								<p>
+									A dataset for building an Airbnb
+									style house aggregation search.
+								</p>
+							</div>
+							<div>
+								<Link
+									primary
+									href="https://opensource.appbase.io/reactivesearch/demos/airbeds/"
+								>
+									Browse
+								</Link>
+							</div>
+						</ImageCard>
+						<ImageCard big src="/live/assets/img/movie.png">
+							<div>
+								<Title>Movies data set</Title>
+								<p>
+									A dataset of 14,000 movies.
+								</p>
+							</div>
+							<div>
+								<Link
+									primary
+									href="https://opensource.appbase.io/reactivesearch/demos/airbeds/"
+								>
+									Browse
+								</Link>
+							</div>
+						</ImageCard>
+						<ImageCard big src="/live/assets/img/dataset.png">
+							<div>
+								<Title>Your Data Set</Title>
+								<p>
+									Submit your own data set
+								</p>
+							</div>
+							<div>
+								<Link
+									primary
+									href="https://opensource.appbase.io/reactivesearch/demos/airbeds/"
+								>
+									Browse
+								</Link>
+							</div>
+						</ImageCard>
+					</Grid>
+				</Section>
 			</section>
 		);
 	}
