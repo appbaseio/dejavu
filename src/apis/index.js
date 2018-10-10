b97dd5d66164d4b24846121c9287f82ce868b467
Divyanshu Maithani <div.blackcat@gmail.com>
2018-10-10 13:28:55 +0530

    :sparkles: Add data apis

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,6 @@
 import testConnection from './app';
 import fetchMappings from './mappings';
 import setCellValue from './cell';
+import addData from './data';
 
-export { testConnection, fetchMappings, setCellValue };
+export { testConnection, fetchMappings, setCellValue, addData };
