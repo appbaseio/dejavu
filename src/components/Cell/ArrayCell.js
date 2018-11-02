fc32eef19d1e6f9b51f4da505aaa2a829adadf40
lakhansamani <lakhan.m.samani@gmail.com>
2018-11-02 13:52:42 +0530

    Support addition of new item in array cell

--- src/components/Cell/ArrayCell.js
+++ src/components/Cell/ArrayCell.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Select, Popover } from 'antd';
 import { func, string, any } from 'prop-types';
 
@@ -12,61 +12,78 @@ type Props = {
 	mode: string,
 };
 
+type State = {
+	data: any,
+};
+
 const { Option } = Select;
 
-const ArrayCell = ({ children, onChange, mode }: Props) => (
-	<Fragment>
-		{mode === 'edit' ? (
-			<Select
-				value={children}
-				css={{
-					width: '100% !important',
-					height: '100% !important',
-					display: 'flex',
-					justifyContent: 'left',
-					alignItems: 'center',
-					'.ant-select-selection': {
-						borderColor: 'transparent',
-						height: '100% !important',
-						width: '100% !important',
-					},
-					'.ant-select-selection__choice': {
-						height: '100% !important',
-					},
-				}}
-				mode="multiple"
-				showSearch
-				autoClearSearchValue
-				maxTagCount={0}
-				onChange={value => onChange(value)}
-				notFoundContent=""
-			>
-				{children.map(child => (
-					<Option key={child}>{child}</Option>
-				))}
-			</Select>
-		) : (
-			Boolean(children.length) && (
-				<Popover
-					content={
-						<div
-							css={{
-								maxWidth: '400px',
-								maxHeight: '300px',
-								overflow: 'auto',
-							}}
+class ArrayCell extends Component<Props, State> {
+	state = {
+		data: this.props.children || [],
+	};
+
+	handleChange = (value: any) => {
+		this.setState({ data: value });
+		this.props.onChange(value);
+	};
+
+	render() {
+		const { data } = this.state;
+		const { mode } = this.props;
+		return (
+			<Fragment>
+				{mode === 'edit' ? (
+					<Select
+						value={data}
+						css={{
+							width: '100% !important !important',
+							height: '100% !important !important',
+							display: 'flex',
+							justifyContent: 'left',
+							alignItems: 'center',
+							'.ant-select-selection': {
+								borderColor: 'transparent !important',
+								height: '100% !important',
+								width: '100% !important',
+							},
+							'.ant-select-selection__choice': {
+								height: '100% !important',
+							},
+						}}
+						mode="tags"
+						maxTagCount={0}
+						onChange={value => this.handleChange(value)}
+						notFoundContent=""
+					>
+						{data.map(child => (
+							<Option key={child}>{child}</Option>
+						))}
+					</Select>
+				) : (
+					Boolean(data.length) && (
+						<Popover
+							content={
+								<div
+									css={{
+										maxWidth: '400px',
+										maxHeight: '300px',
+										overflow: 'auto',
+									}}
+								>
+									<JsonView json={data} />
+								</div>
+							}
+							trigger="click"
 						>
-							<JsonView json={children} />
-						</div>
-					}
-					trigger="click"
-				>
-					<span css={{ cursor: 'pointer' }}> [...] </span>
-				</Popover>
-			)
-		)}
-	</Fragment>
-);
+							<span css={{ cursor: 'pointer' }}> [...] </span>
+						</Popover>
+					)
+				)}
+			</Fragment>
+		);
+	}
+}
 
 ArrayCell.propTypes = {
 	onChange: func.isRequired,
--- src/components/DataTable/Cell.style.js
+++ src/components/DataTable/Cell.style.js
@@ -29,7 +29,7 @@ const Cell = styled('td')`
 	${({ isEditing }) =>
 		isEditing &&
 		css`
-			height: 35px !important;
+			height: 50px !important;
 		`};
 `;
 
