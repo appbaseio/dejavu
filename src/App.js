9ec665bc87836d902e92a6fdb1881a83d98ceca2
lakhansamani <lakhan.m.samani@gmail.com>
2018-11-13 15:33:58 +0530

    Add 404 route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Layout } from 'antd';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import Dejavu from './components/Dejavu';
@@ -8,6 +8,7 @@ import Importer from './components/Importer';
 import SearchPreview from './components/SearchPreview';
 import Mappings from './components/Mappings';
 import Navigation from './components/Navigation';
+import NoMatch from './components/NoMatch';
 
 import configureStore from './store';
 import {
@@ -82,19 +83,26 @@ class App extends Component {
 										paddingBottom: 60,
 									}}
 								>
-									<Route exact path="/" component={Dejavu} />
-									<Route
-										path="/import"
-										component={Importer}
-									/>
-									<Route
-										path="/preview"
-										component={SearchPreview}
-									/>
-									<Route
-										path="/mappings"
-										component={Mappings}
-									/>
+									<Switch>
+										<Route
+											exact
+											path="/"
+											component={Dejavu}
+										/>
+										<Route
+											path="/import"
+											component={Importer}
+										/>
+										<Route
+											path="/preview"
+											component={SearchPreview}
+										/>
+										<Route
+											path="/mappings"
+											component={Mappings}
+										/>
+										<Route component={NoMatch} />
+									</Switch>
 								</div>
 							</Content>
 						</Layout>
