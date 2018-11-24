94ae3d0a5f047e42ed2960560e252dc05e8c2338
lakhansamani <lakhan.m.samani@gmail.com>
2018-11-24 15:46:19 +0530

    Add custom message for mixed content

--- src/apis/app.js
+++ src/apis/app.js
@@ -40,18 +40,25 @@ const testConnection = async (appname, rawUrl) => {
 		}
 		return res;
 	} catch (error) {
-		console.log(error);
 		const err = error;
+		let description = `<b> Possible Errors </b>
+		<ul><li>Invalid connection string or index name </li>
+		<li> Please check is Elasticsearch cluster is up and running</li></ul>`;
+
 		if (err.message === 'Failed to fetch') {
 			err.message = defaultError;
+		}
+
+		if (err.message === 'NetworkError when attempting to fetch resource.') {
+			description = `You are trying to load http content over https.
+				You might have to enable mixed content of your browser
+				<a target="_blank" href="https://kb.iu.edu/d/bdny">https://kb.iu.edu/d/bdny</a>`;
 		}
 
 		throw new CustomError(
 			err.description && !isEmptyObject(JSON.parse(err.description))
 				? err.description
-				: `<b> Possible Errors </b>
-				<ul><li>Invalid connection string or index name </li>
-				<li> Please check is Elasticsearch cluster is up and running</li></ul>`,
+				: description,
 			err.message || defaultError,
 			err.stack,
 		);
