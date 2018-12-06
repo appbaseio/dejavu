e695a1f2fcab2367bd0133e0d962945889ca77e9
Lakhan Samani <lakhan.m.samani@gmail.com>
2018-12-06 23:37:43 +0530

    Update cross browser permissions

--- app/src/index.js
+++ app/src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 CrossStorageHub.init([
 	{ origin: /\.appbase.io$/, allow: ['get'] },
 	{ origin: /hardcore-hoover-0d4fea.netlify.com$/, allow: ['get'] },
-	{ origin: /localhost:1359$/, allow: ['get'] },
+	{ origin: /localhost:135$/, allow: ['get'] },
 ]);
 
 ReactDOM.render(<App />, document.getElementById('root'));
