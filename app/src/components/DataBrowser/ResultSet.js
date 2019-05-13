2202723b5e95d6aebb6418212de7987871e49be1
lakhansamani <lakhan.m.samani@gmail.com>
2019-05-13 15:52:49 +0530

    fix: onQueryChange prop trigger after reload

--- app/src/components/DataBrowser/ResultSet.js
+++ app/src/components/DataBrowser/ResultSet.js
@@ -3,8 +3,9 @@
 import React from 'react';
 import { ReactiveList } from '@appbaseio/reactivesearch';
 import { connect } from 'react-redux';
-import { css } from 'react-emotion';
+import styled, { css } from 'react-emotion';
 import { mediaMin } from '@divyanshu013/media';
+import { Icon } from 'antd';
 
 import DataTable from '../DataTable';
 import Flex from '../Flex';
@@ -30,6 +31,17 @@ import {
 } from '../../actions';
 import colors from '../theme/colors';
 
+const InfoContainer = styled(Flex)`
+	min-height: 150px;
+	position: absolute;
+	top: 0;
+	left: 2px;
+	right: 2px;
+	bottom: 2px
+	z-index: 1000px;
+	background: ${colors.white};
+`;
+
 type Props = {
 	reactiveListKey: number,
 	sortField: string,
@@ -50,155 +62,192 @@ type Props = {
 	version: number,
 };
 
-const ResultSet = ({
-	reactiveListKey,
-	sortField,
-	sortOrder,
-	pageSize,
-	termsAggregationColumns,
-	onSelectedRows,
-	onSetUpdatingRow,
-	onSetQuery,
-	mappings,
-	appname,
-	height,
-	width,
-	headerRef,
-	onSetSelectAll,
-	onSetApplyQuery,
-	onSetStats,
-	version,
-}: Props) => {
-	const { results } = getUrlParams(window.location.search);
-	const currentPage = parseInt(results || 1, 10);
-	return (
-		<ReactiveList
-			key={String(reactiveListKey)}
-			componentId="results"
-			dataField={sortField}
-			sortBy={sortOrder}
-			pagination
-			size={pageSize}
-			showResultStats
-			URLParams
-			currentPage={currentPage}
-			defaultQuery={() => ({
-				sort: [
-					{
-						[sortField]: { order: sortOrder },
-						[version > 5 ? '_id' : '_uid']: { order: sortOrder },
-					},
-				],
-			})}
-			react={{
-				and: [
-					'GlobalSearch',
-					...termsAggregationColumns,
-					'indexField',
-					'typeField',
-				],
-			}}
-			style={{
-				position: 'relative',
-				overflow: 'visible',
-			}}
-			innerClass={{
-				poweredBy: css({
-					display: 'none',
-				}),
-				pagination: css({
-					position: 'fixed',
-					right: 40,
-					zIndex: 105,
-					'.active': {
-						backgroundColor: `${colors.primary} !important`,
-					},
-				}),
-			}}
-			loader={
-				<Flex
-					justifyContent="center"
-					css={{
-						minHeight: 150,
-						position: 'absolute',
-						top: 0,
-						left: 2,
-						right: 2,
-						bottom: 2,
-						zIndex: 1000,
-						background: colors.white,
-					}}
-					id="spinner"
-				>
-					<Loader />
-				</Flex>
-			}
-			onPageChange={() => {
-				onSelectedRows([]);
-				onSetUpdatingRow(null);
-				onSetSelectAll(false);
-				onSetApplyQuery(false);
-			}}
-			renderAllData={({ results: data }) =>
-				data.length ? (
-					<DataTable
-						key={data.length ? data[0]._id : '0'}
-						data={data}
-						mappings={mappings[appname]}
-						height={height}
-						width={width}
-						headerRef={headerRef}
-					/>
-				) : null
-			}
-			onResultStats={stats => {
-				const { totalResults, ...rest } = stats;
-				onSetStats({
-					totalResults:
-						typeof totalResults === 'object'
-							? totalResults.value
-							: totalResults,
-					...rest,
-				});
-			}}
-			renderResultStats={stats => (
-				<Flex
-					justifyContent="center"
-					alignItems="center"
-					css={{
-						display: 'none',
-						[mediaMin.medium]: {
-							display: 'block',
-							position: 'absolute',
-							right: '390px',
-							height: '32px',
-							fontSize: '14px',
-							padding: '0 15px',
-							lineHeight: '1.5',
-							textAlign: 'center',
-							bottom: -45,
-						},
-					}}
-				>
-					Showing <b>{numberWithCommas(stats.displayedResults)}</b> of
-					total{' '}
-					<b>
-						{numberWithCommas(
-							typeof stats.totalResults === 'object'
-								? stats.totalResults.value
-								: stats.totalResults,
-						)}
-					</b>
-				</Flex>
-			)}
-			onQueryChange={(prevQuery, nextQuery) => {
-				onSetQuery(nextQuery);
-				onSetSelectAll(false);
-				onSetApplyQuery(false);
-			}}
-		/>
-	);
+type State = {
+	hasMounted: boolean,
 };
+
+class ResultSet extends React.Component<Props, State> {
+	state = {
+		hasMounted: true,
+	};
+
+	shouldComponentUpdate(nextProps) {
+		if (nextProps.reactiveListKey !== this.props.reactiveListKey) {
+			this.setMountState(false);
+			setTimeout(() => {
+				this.setMountState(true);
+			});
+
+			return false;
+		}
+		return true;
+	}
+
+	setMountState = hasMounted => {
+		this.setState({
+			hasMounted,
+		});
+	};
+
+	render() {
+		const {
+			reactiveListKey,
+			sortField,
+			sortOrder,
+			pageSize,
+			termsAggregationColumns,
+			onSelectedRows,
+			onSetUpdatingRow,
+			onSetQuery,
+			mappings,
+			appname,
+			height,
+			width,
+			headerRef,
+			onSetSelectAll,
+			onSetApplyQuery,
+			onSetStats,
+			version,
+		} = this.props;
+
+		const { results } = getUrlParams(window.location.search);
+		const currentPage = parseInt(results || 1, 10);
+		const { hasMounted } = this.state;
+		return (
+			hasMounted && (
+				<ReactiveList
+					key={String(reactiveListKey)}
+					componentId="results"
+					dataField={sortField}
+					sortBy={sortOrder}
+					pagination
+					size={pageSize}
+					showResultStats
+					URLParams
+					currentPage={currentPage}
+					defaultQuery={() => ({
+						sort: [
+							{
+								[sortField]: { order: sortOrder },
+								[version > 5 ? '_id' : '_uid']: {
+									order: sortOrder,
+								},
+							},
+						],
+					})}
+					react={{
+						and: [
+							'GlobalSearch',
+							...termsAggregationColumns,
+							'indexField',
+							'typeField',
+						],
+					}}
+					style={{
+						position: 'relative',
+						overflow: 'visible',
+					}}
+					innerClass={{
+						poweredBy: css({
+							display: 'none',
+						}),
+						pagination: css({
+							position: 'fixed',
+							right: 40,
+							zIndex: 105,
+							'.active': {
+								backgroundColor: `${colors.primary} !important`,
+							},
+						}),
+					}}
+					onPageChange={() => {
+						onSelectedRows([]);
+						onSetUpdatingRow(null);
+						onSetSelectAll(false);
+						onSetApplyQuery(false);
+					}}
+					onData={({ resultStats }) => {
+						const { numberOfResults, ...rest } = resultStats;
+						onSetStats({
+							totalResults:
+								typeof numberOfResults === 'object'
+									? numberOfResults.value
+									: numberOfResults,
+							...rest,
+						});
+					}}
+					renderResultStats={stats => (
+						<Flex
+							justifyContent="center"
+							alignItems="center"
+							css={{
+								display: 'none',
+								[mediaMin.medium]: {
+									display: 'block',
+									position: 'absolute',
+									right: '390px',
+									height: '32px',
+									fontSize: '14px',
+									padding: '0 15px',
+									lineHeight: '1.5',
+									textAlign: 'center',
+									bottom: -45,
+								},
+							}}
+						>
+							Showing{' '}
+							<b>{numberWithCommas(stats.displayedResults)}</b> of
+							total{' '}
+							<b>
+								{numberWithCommas(
+									typeof stats.numberOfResults === 'object'
+										? stats.numberOfResults.value
+										: stats.numberOfResults,
+								)}
+							</b>
+						</Flex>
+					)}
+					onQueryChange={(prevQuery, nextQuery) => {
+						onSetQuery(nextQuery);
+						onSetSelectAll(false);
+						onSetApplyQuery(false);
+					}}
+				>
+					{({ data, loading, error }) => (
+						<>
+							{loading && (
+								<InfoContainer
+									justifyContent="center"
+									css={{}}
+									id="spinner"
+								>
+									<Loader />
+								</InfoContainer>
+							)}
+							{error && (
+								<InfoContainer flexDirection="column">
+									<Icon type="meh" />
+									<h3>Sorry something went wrong</h3>
+									<pre>{JSON.stringify(error)}</pre>
+								</InfoContainer>
+							)}
+							{data && data.length ? (
+								<DataTable
+									key={data.length ? data[0]._id : '0'}
+									data={data}
+									mappings={mappings[appname]}
+									height={height}
+									width={width}
+									headerRef={headerRef}
+								/>
+							) : null}
+						</>
+					)}
+				</ReactiveList>
+			)
+		);
+	}
+}
 
 const mapStateToProps = state => {
 	const { field: sortField, order: sortOrder } = getSort(state);
