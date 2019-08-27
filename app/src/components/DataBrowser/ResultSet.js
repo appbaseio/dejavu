7ee4dcf48581173633b4ae45f3c1d921fb341925
Siddharth Kothari <sids.aquarius@gmail.com>
2019-08-28 00:59:38 +0530

    patch: revert track_total_hits change
    * as it breaks for v5 indexes (should be only done conditionally for v >= 7)

--- app/src/components/DataBrowser/ResultSet.js
+++ app/src/components/DataBrowser/ResultSet.js
@@ -125,7 +125,6 @@ class ResultSet extends React.Component<Props, State> {
 					URLParams
 					currentPage={currentPage}
 					defaultQuery={() => ({
-						track_total_hits: true,
 						sort: [
 							{
 								[sortField]: { order: sortOrder },
