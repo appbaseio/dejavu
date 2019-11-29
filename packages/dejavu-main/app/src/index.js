fb3f823239fb9ae52c2e63ea7534f50934830991
anuj <anujshah584@gmail.com>
2019-11-29 12:27:38 +0530

    feat: add dejavu webpack-config

---
+++ packages/dejavu-main/app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CrossStorageHub } from 'cross-storage';
+
+const App = () => <div>hello world</div>;
+
+CrossStorageHub.init([
+  { origin: /\.appbase.io$/, allow: ['get'] },
+  { origin: /localhost:1359$/, allow: ['get'] },
+  { origin: /localhost:1358$/, allow: ['get'] },
+]);
+
+ReactDOM.render(<App />, document.getElementById('root'));
