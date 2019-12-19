94790b95f562b80d9875815467bf1d94bfa35ea2
jyash97 <jyash97@gmail.com>
2019-12-19 11:02:10 +0530

    fix: protocol

--- app/src/components/PromotedResultsQueries/utils.js
+++ app/src/components/PromotedResultsQueries/utils.js
@@ -1,5 +1,5 @@
 export default function getPromotedURL(url) {
 	const parsedURL = url.split('@');
-	const protocol = url.split('://');
+	const protocol = url.split('://')[0];
 	return `${protocol}://${parsedURL[1]}`;
 }
