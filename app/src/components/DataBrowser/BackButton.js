7ba8282d7ec1220068ee1e1843434ed0968608e6
jyash97 <jyash97@gmail.com>
2020-01-17 12:55:30 +0530

    use Link instead of history

--- app/src/components/DataBrowser/BackButton.js
+++ app/src/components/DataBrowser/BackButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Icon } from 'antd';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { css } from 'emotion';
 
 const buttonStyles = css`
@@ -10,13 +10,15 @@ const buttonStyles = css`
 `;
 
 const BackButton = props => {
-	const { history, location } = props;
+	const { location } = props;
 	const listingPageURL = `promoted-results-queries/${location.search}`;
 	return (
-		<Button css={buttonStyles} onClick={() => history.push(listingPageURL)}>
-			<Icon type="arrow-left" />
-			Back to Query Rules
-		</Button>
+		<Link to={listingPageURL}>
+			<Button css={buttonStyles}>
+				<Icon type="arrow-left" />
+				Back to Query Rules
+			</Button>
+		</Link>
 	);
 };
 
