01090906dedfd4d9229fcee13854fe3ce5346925
Siddharth Kothari <sids.aquarius@gmail.com>
2022-01-05 09:04:34 +0530

    chore(fix): update babel/preset-env to fix a potential build error

--- packages/browser/.babelrc.js
+++ packages/browser/.babelrc.js
@@ -2,32 +2,33 @@ const { NODE_ENV } = process.env;
 const isProduction = NODE_ENV === 'production';
 
 const presets = [
-  '@babel/preset-react',
-  '@babel/preset-flow',
-  [
-    '@babel/env',
-    {
-      targets: {
-        edge: '17',
-        firefox: '60',
-        chrome: '67',
-        safari: '11.1',
-      },
-      useBuiltIns: 'usage',
-    },
-  ],
+	'@babel/preset-react',
+	'@babel/preset-flow',
+	[
+		'@babel/env',
+		{
+			targets: {
+				edge: '17',
+				firefox: '60',
+				chrome: '67',
+				safari: '11.1',
+			},
+			useBuiltIns: 'usage',
+		},
+	],
 ];
 
 const plugins = [
-  [
-    'emotion',
-    isProduction ? { hoist: true } : { sourceMap: true, autoLabel: true },
-  ],
-  '@babel/plugin-proposal-class-properties',
-  '@babel/plugin-transform-spread',
-  '@babel/plugin-proposal-object-rest-spread',
-  '@babel/plugin-syntax-dynamic-import',
-  '@babel/plugin-proposal-export-default-from',
+	[
+		'emotion',
+		isProduction ? { hoist: true } : { sourceMap: true, autoLabel: true },
+	],
+	'@babel/plugin-proposal-class-properties',
+	'@babel/plugin-transform-spread',
+	'@babel/plugin-proposal-object-rest-spread',
+	'@babel/plugin-syntax-dynamic-import',
+	'@babel/plugin-proposal-export-default-from',
+	'@babel/plugin-proposal-optional-chaining',
 ];
 
 module.exports = { presets, plugins };
