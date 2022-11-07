7c64221da607c9d987f55297e71e9368225d3b53
Shubham Shah <shubhamshahrising@gmail.com>
2022-11-07 23:16:12 +0530

    chore: use refactored function

--- packages/browser/src/apis/cell.js
+++ packages/browser/src/apis/cell.js
@@ -1,38 +1,18 @@
 import { unflatten } from 'flat';
 
-import {
-	parseUrl,
-	getCustomHeaders,
-	convertArrayToHeaders,
-	getHeaders,
-} from '../utils';
 import CustomError from '../utils/CustomError';
+import { updateDocument } from './data';
 
 const setCellValue = async (app, type, rawUrl, id, property, value) => {
-	const defaultError = 'Unable to update data';
+	const doc = unflatten({ [property]: value });
+	const formattedId = encodeURIComponent(id);
 	try {
-		const { url } = parseUrl(rawUrl);
-		const headers = getHeaders(rawUrl);
-		const customHeaders = getCustomHeaders(app);
-		const doc = unflatten({ [property]: value });
-		const formattedId = encodeURIComponent(id);
-
-		const res = await fetch(`${url}/${app}/_update/${formattedId}`, {
-			headers: {
-				...headers,
-				...convertArrayToHeaders(customHeaders),
-			},
-			method: 'POST',
-			body: JSON.stringify({ doc }),
-		}).then(response => response.json());
-
-		if (res.status >= 400) {
-			throw new CustomError(
-				JSON.stringify(res.error || res, null, 2),
-				`HTTP STATUS: ${res.status} - ${res.message || defaultError}`,
-			);
-		}
-		return res;
+		return updateDocument({
+			index: app,
+			id: formattedId,
+			url: rawUrl,
+			document: doc,
+		});
 	} catch (error) {
 		const errorMessage = error.message || 'Unable to update data';
 
