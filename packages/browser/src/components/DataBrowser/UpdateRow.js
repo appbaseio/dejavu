dd32960ee464317b900924c0c40de36442ddc9b3
Shubham Shah <shubhamshahrising@gmail.com>
2022-12-08 15:33:33 +0530

    fix: field out of place in form

--- packages/browser/src/components/DataBrowser/UpdateRow.js
+++ packages/browser/src/components/DataBrowser/UpdateRow.js
@@ -138,12 +138,12 @@ class UpdateRowModal extends Component<Props, State> {
 				>
 					<Row>
 						<Col span={12}>
-							<Item label="Index" css={{ marginRight: '10px' }}>
+							<Item label="Index" css={{ marginRight: '15px' }}>
 								<Input name="index" value={index} disabled />
 							</Item>
 						</Col>
 						<Col span={12}>
-							<Item label="Type">
+							<Item label="Type" css={{ marginRight: '15px' }}>
 								<Input name="type" value={type} disabled />
 							</Item>
 						</Col>
